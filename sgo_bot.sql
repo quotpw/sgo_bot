ca4cbab74a5fd3b6fb6353bf5f7825a5fdc5c435
edman <edman@quot.pw>
2022-02-28 18:28:11 +0300

    Change sql query

--- sgo_bot.sql
+++ sgo_bot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: db
--- Время создания: Фев 28 2022 г., 11:40
+-- Время создания: Фев 28 2022 г., 15:23
 -- Версия сервера: 8.0.28
 -- Версия PHP: 8.0.16
 
@@ -12,14 +12,16 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База данных: `sgo_bot`
 --
+CREATE DATABASE IF NOT EXISTS `sgo_bot` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `sgo_bot`;
 
 -- --------------------------------------------------------
 
@@ -27,17 +29,20 @@ SET time_zone = "+00:00";
 -- Структура таблицы `accounts`
 --
 
-CREATE TABLE `accounts` (
-  `id` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `state_id` int NOT NULL,
-  `city_id` int NOT NULL,
-  `org_type` int NOT NULL,
-  `org_id` int NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `session` json DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `accounts`
+(
+    `id`       int         NOT NULL,
+    `name`     varchar(50) NOT NULL,
+    `state_id` int         NOT NULL,
+    `city_id`  int         NOT NULL,
+    `org_type` int         NOT NULL,
+    `org_id`   int         NOT NULL,
+    `username` varchar(30) NOT NULL,
+    `password` varchar(32) NOT NULL,
+    `session`  json DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -45,13 +50,16 @@ CREATE TABLE `accounts` (
 -- Структура таблицы `homeworks`
 --
 
-CREATE TABLE `homeworks` (
-  `id` int NOT NULL,
-  `account_id` int NOT NULL,
-  `class_meeting_id` int NOT NULL,
-  `value` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `homeworks`
+(
+    `id`               int       NOT NULL,
+    `account_id`       int       NOT NULL,
+    `class_meeting_id` int       NOT NULL,
+    `value`            text      NOT NULL,
+    `date`             timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -59,11 +67,14 @@ CREATE TABLE `homeworks` (
 -- Структура таблицы `users`
 --
 
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `user_id` bigint NOT NULL,
-  `account_id` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `users`
+(
+    `id`         int    NOT NULL,
+    `user_id`    bigint NOT NULL,
+    `account_id` int DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 --
 -- Индексы сохранённых таблиц
@@ -73,21 +84,21 @@ CREATE TABLE `users` (
 -- Индексы таблицы `accounts`
 --
 ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `homeworks`
 --
 ALTER TABLE `homeworks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `account_id` (`account_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `account_id` (`account_id`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `account_id_idx` (`account_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `account_id_idx` (`account_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -97,19 +108,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `homeworks`
 --
 ALTER TABLE `homeworks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -119,15 +130,15 @@ ALTER TABLE `users`
 -- Ограничения внешнего ключа таблицы `homeworks`
 --
 ALTER TABLE `homeworks`
-  ADD CONSTRAINT `homeworks_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+    ADD CONSTRAINT `homeworks_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Ограничения внешнего ключа таблицы `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
+    ADD CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
