f7edac39c77eafba8c7d158d1a9660f7aa7974b9
edman <62401294+quotpw@users.noreply.github.com>
2022-03-25 14:42:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ apt update
 apt install supervisor python3 python3-dev python3-pip libmagickwand-dev wget -y #unzip
 
 # Download fonts from jetbrains suite
-#wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip -O fonts.zip
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip -O fonts.zip
 
 # Unzip ttf fonts
-#unzip -j fonts.zip "fonts/ttf/*" -d "/usr/share/fonts/ttf"
+unzip -j fonts.zip "fonts/ttf/*" -d "/usr/share/fonts/ttf"
 
 # Delete archive
-#rm fonts.zip
+rm fonts.zip
 
 # Install python dependencies
 pip3 install -r /root/sgo_bot/requirements.txt
